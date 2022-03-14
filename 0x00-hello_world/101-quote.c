3b4a9a7838f8f15eb435b825acb524cc5f880005
Natenel42 <natenelassefa801@gmail.com>
2022-03-14 11:07:20 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
-#include<stdio.h> 
-#include<unistd.h>  
+#include <stdio.h> 
+#include <unistd.h>  
 /** 
-*main-Entry point 
-*Return: Always 1 
+*main - Entry point 
+*Return: Always 1 (Success) 
 * 
 */  
 int main(void) 
