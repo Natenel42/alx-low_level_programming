3f6490f8c8b180d3da34c2c163a7e02723239c84
Natenel42 <natenelassefa801@gmail.com>
2022-03-14 12:16:41 -0700

    space

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <studio.h>
+#include <stdlib.h>
 /**
- *main - Entry point
- *Return: Always 1 (Success)
- *
+ * main -Entry point
+ * Return: Always 1 (Succes)
  */
-int main(void){
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
-
+int main(void)
+{
+	Write(STUDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
 }
