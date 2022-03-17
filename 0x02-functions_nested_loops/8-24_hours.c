1056439144f80a0152d0af71d56aa68f5de303f2
Natenel42 <natenelassefa801@gmail.com>
2022-03-17 12:46:42 -0700

    one

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,11 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar('0' + hour / 10);
-			_putchar('0' + hour % 10);
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
 			_putchar(':');
-			_putchar('0' + minute / 10);
-			_putchar('0' + minute % 10);
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 +'0');
 			_putchar('\n');
 		}
 	}
