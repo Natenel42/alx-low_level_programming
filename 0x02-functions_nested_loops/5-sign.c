a0737269ce894fb58e1aa43d596fb681e7f252fd
Natenel42 <natenelassefa801@gmail.com>
2022-03-17 11:10:06 -0700

    one

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - check sign of a number.
  *
@@ -8,20 +7,20 @@
  */
 int print_sign(int n)
 {
-  if(n > 0)
-  {
-    _putchar('+');
-    return (1);
-  } 
-  else if (n == 0)
-  {
-    _putchar('0');
-    return (0);
-  }
-  else if (n < 0)
-  {
-    _putchar('-');
-    return (-1);
-  }
-  return (0);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
 }
