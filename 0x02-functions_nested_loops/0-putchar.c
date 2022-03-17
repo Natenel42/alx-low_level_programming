cfbb843db9de3d9cfc43b9f49eccaf010e8ab3e2
Natenel42 <natenelassefa801@gmail.com>
2022-03-16 21:47:44 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char *main = "_putchar";
+
+  while (*main)
+  {
+    _putchar(*main);
+    main++;
+  }
+  _putchar('\n');
+  return (0);
+}
