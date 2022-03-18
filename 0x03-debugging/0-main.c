012a78b31f4eb4713dd8b94c720a291ffa0ce3f4
Natenel42 <natenelassefa801@gmail.com>
2022-03-18 14:43:40 -0700

    one

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#includ "main.h"
+
+/**
+ * main - tests function that prints if integer if posetive and negative
+ *
+ * Return: 0
+ */
+
+int mian(void)
+{
+	int i;
+
+	i = 0;
+	posetive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is dubagging project
