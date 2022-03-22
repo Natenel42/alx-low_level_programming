0d89a4ad60a89a2048ce3820d4ea12cb1801e07d
Natenel42 <natenelassefa801@gmail.com>
2022-03-22 15:33:22 -0700

    five

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _atoi - a functionthat convert aschii to integer
+ * @s: the string to be converted to integer
+ * Return: intger
+ */
+in _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
