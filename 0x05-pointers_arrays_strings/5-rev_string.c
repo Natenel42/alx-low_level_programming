c33347286294e3029c82173cbc59645d5c1ce05b
Natenel42 <natenelassefa801@gmail.com>
2022-03-22 14:43:44 -0700

    five

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,11 @@ void rev_string(char *s)
 	int len, i, half;
 	char temp;
 
-	for (len = 0; s[len] != '\0'; len++);
+	for (len = 0; s[len] != '\0'; len++)
+		;
 
 	i = 0;
-	
 	half = len / 2;
-	
 	while (half--)
 	{
 		temp = s[len - i - 1];
