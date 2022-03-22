7b74ab5aefee4d455628e3da7d1425ab2cfc2b61
Natenel42 <natenelassefa801@gmail.com>
2022-03-22 13:52:35 -0700

    five

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the bvlaur in points to 98.
+ * reset_to_98 - takes a pointer to an int as parameter 
+ * and updates the bvlaur in points to 98.
  *
  * @n: pointer to n
  * Return: void
