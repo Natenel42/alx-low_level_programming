c1504c6fe82ae1c6d65a044059eb39d5e9b26f68
Natenel42 <natenelassefa801@gmail.com>
2022-03-28 14:11:19 -0700

    seven

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,33 @@
 #include "main.h"
-#include <stdlib.h>
 /**
-  *_strpbrk - seraches a string for any of a set of bytes.
-  *it locates the first occurence in the string s of any of
-  *the bytes in the string accept.
-  *@s: pointer to string.
-  *@accept: pointer to strin to be matched.
+  *_strstr - locates a substring.
+  *finds first occurence of the substring needle in
+  *haystack.
+  *@haystack: pointer to string.
+  *@needle: pointer to substring.
   *
-  *Return: pointer to byte in s that matches one of the bytes
-  *in accept ot NULL if no such byte is found.
+  *Return: pointer to beginning of located string
+  *or NULL if substring isnt located.
   */
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-	char *p;
-	int counter, counter2;
-
-	p = 0;
-
-	for (counter = 0; s[counter] != '\0'; counter++)
+	while (*haystack)
 	{
-		for (counter2 = 0; accept[counter2]; counter2++)
+		char *start = haystack;
+		char *needlestr = needle;
+
+		while (*needlestr && *haystack && *haystack == *needlestr)
 		{
-			if (accept[counter2] == s[counter])
-			{
-				p = &s[counter];
-				return (p);
-			}
+			haystack++;
+			needlestr++;
 		}
+
+		if (!*needlestr)
+		{
+			return (start);
+		}
+
+		haystack = start + 1;
 	}
-	return (p);
+	return (0);
 }
